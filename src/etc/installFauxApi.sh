d010ee3052f2c85479f65abfb0de8a5b29145edb
Nakul Bali <nakul.bali@hotmail.com>
2020-08-01 00:07:21 +0200

    fixing fauxapi install

--- src/etc/installFauxApi.sh
+++ src/etc/installFauxApi.sh
@@ -1,13 +1,13 @@
 echo "Installing Faux API";
-			set fauxapi_base_package_url='https://raw.githubusercontent.com/ndejong/pfsense_fauxapi_packages/master'
-            set fauxapi_latest=`fetch -qo - ${fauxapi_base_package_url}/LATEST`
-            fetch ${fauxapi_base_package_url}/${fauxapi_latest}
-			pkg-static install ${fauxapi_latest}
-			echo "Faux API installed"
-			echo "Setting Faux API credentials"
-			cp /etc/fauxapi/credentials.sample.ini /etc/fauxapi/credentials.ini
-			echo "[PPFAgatekeeperapiaccess]" >> /etc/fauxapi/credentials.initialis
-			$secret = `head /dev/urandom | base64 -w0 | tr -d /+= | head -c 60`
-			echo "secret = "$secret >> /etc/fauxapi/credentials.ini
-			echo "comment = Gatekeeper Faux API access" >> /etc/fauxapi/credentials.ini
-			echo "Faux API credentials saved"
+fauxapi_base_package_url='https://raw.githubusercontent.com/ndejong/pfsense_fauxapi_packages/master'
+fauxapi_latest=`fetch -qo - $fauxapi_base_package_url/LATEST`
+fetch $fauxapi_base_package_url/$fauxapi_latest
+pkg-static install -y $fauxapi_latest
+echo "Faux API installed"
+echo "Setting Faux API credentials"
+cp /etc/fauxapi/credentials.sample.ini /etc/fauxapi/credentials.ini
+echo "[PPFAgatekeeperapiaccess]" >> /etc/fauxapi/credentials.ini
+$secret = `head /dev/urandom | base64 | tr -d /+= | head -c 60`
+echo "secret = "$secret >> /etc/fauxapi/credentials.ini
+echo "comment = Gatekeeper Faux API access" >> /etc/fauxapi/credentials.ini
+echo "Faux API credentials saved"
